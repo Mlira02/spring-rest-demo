95bdcf1e8831d21e2524e426e4bd448f34ddd878
Marcos Lira <liram1995@gmail.com>
2021-04-28 09:54:15 -0500

    Added rest controller folder and one rest controller

---
+++ src/main/java/com/marcos/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.marcos.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.marcos.springdemo")
+public class DemoAppConfig
+{
+}
