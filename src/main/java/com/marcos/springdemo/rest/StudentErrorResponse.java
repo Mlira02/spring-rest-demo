4d404db6f799fd4e9400aa75b72b8570deed0452
Mlira02 <liram1995@gmail.com>
2021-05-17 10:51:34 -0500

    Added exception handling to getStudent mapping

---
+++ src/main/java/com/marcos/springdemo/rest/StudentErrorResponse.java
@@ -0,0 +1,50 @@
+package com.marcos.springdemo.rest;
+
+public class StudentErrorResponse
+{
+    private int status;
+    private String message;
+    private long timeStamp;
+
+    public StudentErrorResponse()
+    {
+
+    }
+
+public StudentErrorResponse(int status, String message, long timeStamp)
+    {
+    this.status = status;
+    this.message = message;
+    this.timeStamp = timeStamp;
+    }
+
+public int getStatus()
+    {
+    return status;
+    }
+
+public long getTimeStamp()
+    {
+    return timeStamp;
+    }
+
+public String getMessage()
+    {
+    return message;
+    }
+
+public void setMessage(String message)
+    {
+    this.message = message;
+    }
+
+public void setStatus(int status)
+    {
+    this.status = status;
+    }
+
+public void setTimeStamp(long timeStamp)
+    {
+    this.timeStamp = timeStamp;
+    }
+}
